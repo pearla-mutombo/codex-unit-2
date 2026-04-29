167426bbaef9d8705478c82bb6e044e7ec5647b2
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-29 15:44:20 -0400

    finished level 4 initialized boolean variables with a question-word names. tested and passed. (only if you use 'let' instead of 'const'.

--- lesson-06-js-syntax/level-04-booleans-variables/script.js
+++ lesson-06-js-syntax/level-04-booleans-variables/script.js
@@ -4,4 +4,19 @@
 // Example:
 // let isLoggedIn = false;
 
-let isLoggedIn = false;
+/* i can also use 'const' instead of 'let'.
+ but it won't let me pass the test 
+ if i use const. therefore i just used
+let in lines 9 - 18 */
+let isLoggedIn = false; 
+let hasAccount = true;
+let canEdit = false;
+let shouldRefresh = true;
+let isPaymentComplete = false;
+let hasFreeShipping = true;
+let canUploadFile = true;
+let shouldChargeUser = false;
+let isInDarkMode = true;
+let hasWifiConnection = true;
+
+export default hasWifiConnection;
