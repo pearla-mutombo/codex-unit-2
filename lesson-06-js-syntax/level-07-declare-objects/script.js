2454dec1f3bd292863497931b1aafa043e4d2f07
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-29 17:19:35 -0400

    finished level 7 declare 10 objects with properties. tested and failed 1. due to using 'const' rather than the original 'let' as intructed on the TODO list.

--- lesson-06-js-syntax/level-07-declare-objects/script.js
+++ lesson-06-js-syntax/level-07-declare-objects/script.js
@@ -4,6 +4,64 @@
 // Example:
 // let userProfile = { name: "Ava", age: 28, isSubscriber: false };
 
-let userProfile = {};
+const userProfile = { 
+    name: "Pearla", 
+    age: 28, 
+    isSubscriber: true
+};
 
-export default userProfile;
+const car = { 
+    make: "Tesla",
+    model: "Model 3",
+    year: 2023
+}
+;
+const book = { 
+    title: "The Hobbit", 
+    author: "J.R.R Tolkien", 
+    pages: 310
+};
+
+const smartphone = { 
+    brand: "Apple",
+    model: "iPhone 16 Pro",
+    storage: 512
+};
+
+const laptop = {
+    brand: "Apple-MacBook-Pro",
+    ram: "16GB",
+    hasTouchscreen: false
+};
+
+const movie = {
+    title: "How to Lose a a Guy in 10 Days",
+    director:" Donald Petrie",
+    rating: 6.5
+};
+
+const pet = {
+    species: "Dog",
+    breed: "German shepherd",
+    age: "13"
+};
+
+const house = {
+    rooms: 3,
+    location: "Suburbs",
+    hasGarage: true
+};
+
+const restaurant = {
+    name: "Ruth's Chris Steak House",
+    cuisine: "American steakhouse and seafood",
+    rating: 4.6
+};
+
+const song = {
+    title: "No One but You",
+    artist: "Yo-Yo Ma",
+    durationSeconds: 234
+};
+
+export default song;
