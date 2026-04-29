12765d25f3f2161927a50120784584b6c97ce5ee
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-29 16:06:37 -0400

    finished level 5 declare string and number arrays (same-line, plural names). tested and passed only after replaceing 'const' with the original 'let'.

--- lesson-06-js-syntax/level-05-declare-arrays/script.js
+++ lesson-06-js-syntax/level-05-declare-arrays/script.js
@@ -4,4 +4,20 @@
 // Example:
 // let userNames = ["Ava", "Jon", "Maya"];
 
-let userNames = [];
+/* when i tested with 'const' it won't let me pass. 
+only when i replace 'const' with 'let' would it let me "passed".*/
+/* 5 Plural-named string arrays */
+let userNames = [ "Tom", "Dick", "Harry"];
+let favoriteCities = ["Tokyo", "Paris", "Dubai"];
+let favoriteGenres = ["Sci-fi", "Comedy","Action"];
+let fruitNames = ["Strawberry", "Green-Apple", "Mango"];
+let codingLanaguages = ["JavaScript", "Syntax", "Boolean"];
+
+/*5 Plural-named number arrays */
+let luckyNumbers = [5, 18, 21];
+let gameScores = [150, 350, 500];
+let birthYears = [1984, 1995, 2001];
+let prices = [9.99, 18.99, 39.99];
+let zipCodes = [90120, 10001, 30305];
+
+export default favoriteCities;
