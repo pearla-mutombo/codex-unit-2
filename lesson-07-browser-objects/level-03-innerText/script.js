18fdde2103637396fbe9f72abe21ce0ca72c2be8
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-30 14:14:31 -0400

    finished level 3 innerText

--- lesson-07-browser-objects/level-03-innerText/script.js
+++ lesson-07-browser-objects/level-03-innerText/script.js
@@ -1,6 +1,16 @@
 // Level 03 — innerText starter
 // TODO: select an element (e.g. document.getElementById('desc'))
+const pTag = document.getElementById("desc");
 // TODO: console.log the previous innerText, set a new innerText, then log the new value
+//console log innerText properties
+console.log(pTag.innerText);
+pTag.innerText = "Good afternoon world!"
+
+
+
+
+
+
 
 // Scaffold:
 // const el = document.getElementById('desc');
