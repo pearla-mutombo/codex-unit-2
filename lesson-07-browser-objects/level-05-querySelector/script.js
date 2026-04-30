eba7bbb6d0cab2c8ed665e0bb2665169feb762bc
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-30 15:56:14 -0400

    finished level 5 querySelector

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,6 +1,30 @@
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
+
+// for .item
+const liTag = document.querySelector(".item");
+liTag.innerHTML = "<h1>You want the Blue pill or the Red pill?</h1>";
+console.log(liTag.innerHTML);
+
 // TODO: log previous content and update nodes (iterate NodeList with forEach)
+
+//  Before: for #content log previous values
+const one= document.querySelector("#content");
+pTag.innerHTML = "The oracle said NEO was the ONE";
+console.log(one.innerHTML);
+
+// // After: for #content 
+// const list = document.querySelectorAll("#content");
+// list.forEach((el, index) => {
+//     listinnerText = "Content" + (index + 1)
+// });
+
+//for .item
+const list = document.querySelectorAll(".item");
+list.forEach((el, index) => {
+    listinnerText = "Item" + (index + 1)
+});
+
 
 // Scaffold:
 // const one = document.querySelector('#content');
