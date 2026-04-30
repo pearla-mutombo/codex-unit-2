54c096d32d1dd41d37a5bde312ce7b090c0d84ca
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-30 13:39:27 -0400

    finished level 1 document object

--- lesson-07-browser-objects/level-01-document-object/script.js
+++ lesson-07-browser-objects/level-01-document-object/script.js
@@ -1,1 +1,9 @@
 debugger;
+/* console log document's title */
+console.log(document.title); 
+/* console log document's URL */
+console.log(document.URL);
+/* console log document's scripts */
+console.log(document.scripts);
+/* console log document's stylesheets */
+console.log(document.styleSheets);
