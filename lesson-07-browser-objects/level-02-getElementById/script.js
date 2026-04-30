0966059ef4b35dbc472f6a123b5c7ecf0df11325
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-30 13:55:57 -0400

    finished level 2 getElementById

--- lesson-07-browser-objects/level-02-getElementById/script.js
+++ lesson-07-browser-objects/level-02-getElementById/script.js
@@ -1,6 +1,17 @@
 // Level 02 — getElementById starter
 // TODO: select the element with document.getElementById('sample')
+ const element = document.getElementById("sample")
+
 // TODO: log properties: id, classList, innerText, innerHTML, attributes
+
+//console log element properties
+console.log(element.id);
+console.log(element.classList);
+console.log(element.innerText);
+console.log(element.innerHTML);
+console.log(element.attributes);
+
+
 
 // Example scaffold (fill in the TODOs):
 // const el = document.getElementById('sample');
