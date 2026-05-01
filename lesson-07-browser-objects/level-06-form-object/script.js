e91870667a10e8658e932aa5aa073791b01ca86a
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-01 15:59:21 -0400

    finished level 6 form object starter

--- lesson-07-browser-objects/level-06-form-object/script.js
+++ lesson-07-browser-objects/level-06-form-object/script.js
@@ -1,7 +1,29 @@
 // Level 06 — form-object starter
 // TODO: select the form element (e.g. document.getElementById('sample-form'))
-// TODO: log form.id, form.name, form.elements.length, form.enctype, form.className
 
+// select the form element
+const form = document.getElementById ("sample-form");
+
+// TODO: log form.id, form.name, form.elements.length, form.enctype, form.className
+if (form) {
+    console.log("sample-form", form.id);
+    console.log("sampleForm", form.name);
+    console.log("0", form.min.length);
+    console.log("10", form.max.length);
+    console.log("radio", form.enctype);
+    console.log("radio", form.enctype);
+    console.log("checkbox", form.enctype);
+    console.log("text", form.enctype);
+    console.log("submit", form.enctype);
+    console.log("message", form.className);
+    console.log("volume", form.className);
+    console.log("color", form.className);
+    console.log("color", form.className);
+    console.log("agree", form.className);
+    console.log("attachment", form.className);
+} else {
+    console.log("Form not found!");
+}
 // Scaffold:
 // const form = document.getElementById('sample-form');
 // if (form) {
