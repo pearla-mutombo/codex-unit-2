776ae487ff9c177b45ecddbd0c69f19807f8b0fc
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-01 13:39:30 -0400

    finished level 10 declare 10 small functions

--- lesson-06-js-syntax/level-10-declare-functions/script.js
+++ lesson-06-js-syntax/level-10-declare-functions/script.js
@@ -4,3 +4,41 @@
 function getUser() {
   return "returns the current user";
 }
+
+function getPassword() {
+  return "returns password";
+}
+
+function getEmail() {
+  return "returns email";
+}
+
+function setUser() {
+  return "set user";
+}
+
+function setPassword() {
+  return "set password";
+}
+
+function setEmail() {
+  return "set email";
+}
+
+function fetchAnimal() {
+  return "fetch the dog";
+}
+
+function fetchColor() {
+  return "fecth the color lightblue";
+}
+
+function fetchZipcode() {
+  return "fetch 90120 zip";
+}
+
+function fetchCities() {
+  return "fetch the location of Tokyo";
+}
+
+export default fetchCities;
