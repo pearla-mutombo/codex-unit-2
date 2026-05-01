4d3cdee7313ad2b0ac81203f67a417a390edd7f9
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-01 15:31:21 -0400

    made corrections on Level 5 querySelector line 17-31 regarding content but didn't find #content in index.html

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,6 +1,9 @@
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
 
+// handle the #content element
+const one = document.querySelector('#content');
+
 // for .item
 const liTag = document.querySelector(".item");
 liTag.innerHTML = "<h1>You want the Blue pill or the Red pill?</h1>";
@@ -8,21 +11,24 @@ console.log(liTag.innerHTML);
 
 // TODO: log previous content and update nodes (iterate NodeList with forEach)
 
-//  Before: for #content log previous values
-const one= document.querySelector("#content");
-pTag.innerHTML = "The oracle said NEO was the ONE";
-console.log(one.innerHTML);
+// log previous content Before updating
+console.log('Before (content:', one? one.innerHTML: "Not found");
 
-// // After: for #content 
-// const list = document.querySelectorAll("#content");
-// list.forEach((el, index) => {
-//     listinnerText = "Content" + (index + 1)
-// });
+if (one) {
+    one.innerHTML = "<h1>The oracle said NEO was the ONE</h1>";
+    console.log('After (content):', one.innerHTML);
+}
+
+// handle the .item elements (NodeList)
+const itemList = document.querySelectorAll(".item");
+
+//iterate through the NodeList using forEach
+itemList.forEach(el, index);
 
 //for .item
 const list = document.querySelectorAll(".item");
 list.forEach((el, index) => {
-    list.innerText = "Item" + (index + 1)
+    el.innerText = "Item" + (index + 1)
 });
 
 
