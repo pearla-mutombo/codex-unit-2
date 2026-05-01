ea878af09aa54327f65d3c03e585eb0ea5e4960b
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-01 13:49:42 -0400

    finished level 11 call provided function with args (10 calls)

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -5,4 +5,24 @@ export function reportScore(playerName, score) {
   return playerName + " scored " + score + " points";
 }
 
-const avaScoreReport = reportScore("Ava", 0);
+const avaScoreReportAva = reportScore("Ava", 50);
+
+const avaScoreReportPreston = reportScore("Preston", 100);
+
+const avaScoreReportZara = reportScore("Zara", 150);
+
+const avaScoreReportNeo = reportScore("Neo", 200);
+
+const avaScoreReportHarry = reportScore("Harry", 250);
+
+const avaScoreReportRon = reportScore("Ron", 300);
+
+const avaScoreReportHermonie = reportScore("Hermonie", 350);
+
+const avaScoreReportMorphius = reportScore("Morphius", 400);
+
+const avaScoreReportSmith = reportScore("Smith", 450);
+
+const avaScoreReportOrecal = reportScore("Orecal", 500);
+
+export default avaScoreReportOrecal;
