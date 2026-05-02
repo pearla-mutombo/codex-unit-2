44386733d5fea2cf9727589d88e6c46759747910
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-02 15:25:13 -0400

    finished level 2 using fetch for GET

--- lesson-08-rest-api/level-02/script.js
+++ lesson-08-rest-api/level-02/script.js
@@ -1,11 +1,13 @@
 // Level 02 starter script
 // TODO: Get & save the form element to `form`, attach onsubmit, and call event.preventDefault().
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
-
+// attach/call the handleSubmit function
+form.onsubmit = handleSubmit;
+//cdeclare the handleSubmit function
 function handleSubmit(event) {
   event.preventDefault();
+  const response = fetch('https://api.jsoning.com/mock/public/users');
+  const resultTag = document.getElementById("result");
   // TODO: set result.innerText to a short explanation about fetch
+  resultTag.innerText = "The fetch function starts a network request, then it accepts a URL to connect to, next it returns a Promise and When the Promise is resolved, you can inspect the response from the server."; 
 }
-
-if (form) form.onsubmit = handleSubmit;
