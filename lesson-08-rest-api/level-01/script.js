d698e77227c307bc7c863f5037c43b8794526de0
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-02 14:21:05 -0400

    finished level 1 what is REST API

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -2,3 +2,4 @@
 // TODO: Select a target element and set its innerText to explain REST API.
 const result = document.getElementById("result");
 // Example: result.innerText = '...'; // fill this in during the exercise
+result.innerText = "REST API uses HTTP-based interface where clients make requests and the servers return a response.";
