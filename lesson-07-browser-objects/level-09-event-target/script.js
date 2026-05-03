a9a7fc2601a3dda6ddc19388919a8f8bf12d0d18
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 14:42:50 -0400

    finished level 9 event.target starter

--- lesson-07-browser-objects/level-09-event-target/script.js
+++ lesson-07-browser-objects/level-09-event-target/script.js
@@ -1,13 +1,23 @@
 // Level 09 — event.target starter
 // TODO: inside submit handler inspect event.target and event.currentTarget
-
-function handleSubmit(event) {
-  // TODO: call event.preventDefault() when experimenting
-  // event.preventDefault();
-  console.log("event.target:", event.target);
-  console.log("event.currentTarget:", event.currentTarget);
-  // TODO: const form = event.target; use form.elements to access inputs
+const form = document.getElementById("sample-form");
+ if (form) {
+  form.onsubmit = handleSubmit;
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  console.log("event.target:", event.target);
+  console.log("event.currentTarget:", event.currentTarget);
+
+  const formTag = event.target;
+  console.log("Form ID via target:", formTag.id);
+}
+ // TODO: call event.preventDefault() when experimenting
+// event.preventDefault();
+// console.log("event.target:", event.target);
+// console.log("event.currentTarget:", event.currentTarget);
+// TODO: const form = event.target; use form.elements to access inputs
 // const form = document.getElementById('sample-form');
 // if (form) form.onsubmit = handleSubmit;
