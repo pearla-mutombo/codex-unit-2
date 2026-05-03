09e17bf9a39af29fa40321e17937ac59c530efa7
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 16:05:57 -0400

    finished level 12 radio-checkbox starter

--- lesson-07-browser-objects/level-12-radio-checkbox/script.js
+++ lesson-07-browser-objects/level-12-radio-checkbox/script.js
@@ -1,11 +1,21 @@
 // Level 12 — radio-checkbox starter
 // TODO: in submit handler read radio via form.elements.color.value and checkbox via form.elements.subscribe.checked
+const form = document.querySelector("#sample-form");
+if (form) {
+  form.onsubmit = handleSubmit;
+}
 
 function handleSubmit(event) {
   // event.preventDefault();
+  event.preventDefault();
   // const form = event.target;
+  const form = event.target;
   // TODO: const color = form.elements.color.value;
+  const color = form.elements.color.value;
   // TODO: const subscribe = form.elements.subscribe.checked;
+  const subscribe = form.elements. subscribe.checked;
+  console.log("Selected color:", color);
+  console.log("Subscribed:", subscribe);
   console.log("radio & checkbox scaffold");
 }
 
