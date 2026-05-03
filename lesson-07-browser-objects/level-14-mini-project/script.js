4ff25b3ac2f8f66fbde8dcee452fdaf24002be35
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 17:20:06 -0400

    finished level 14 - mini-project

--- lesson-07-browser-objects/level-14-mini-project/script.js
+++ lesson-07-browser-objects/level-14-mini-project/script.js
@@ -3,11 +3,42 @@
 
 function handleSubmit(event) {
   // event.preventDefault();
+  event.preventDefault();
+
   // const form = event.target;
+  const form = event.target;
   // TODO: read name, message, volume, selected radio, checkbox
+  // Read values via form.elements
+  const nameValue = form.elements.name.value;
+  const messageValue = form.elements.message.value;
+  // Convert range value in a number
+  const volume = Number(form.elements.volume.value);
+  // Get radio value (it will be "" if nothing is selected)
+  const color = form.elements.color.value;
+  // Get checkbox in a boolean format 
+  const isSubscribed = form.elements.subscribe.checked;
+
   // TODO: validate required fields, display summary, then form.reset();
-  console.log("mini-project scaffold");
+  if (name == "" || message === "" || color === "") {
+    alert ("Please fill out the Nme, Message, and pick a Color!");
+    return; // Stop the function here if validation fails
+  }
+
+  const summary = document.getElementById("summary");
+  if (summary) {
+    summary.innterHTML =
+    "Thank you, ${nameValue}! Your message has been received";
+  }
+  
+  form.reset();
+
+  console.log( "Form processed successfully.");
 }
 
 // const form = document.getElementById('sample-form');
+const form = document.getElementById('sample-form');
 // if (form) form.onsubmit = handleSubmit;
+if (form) {
+  form.onsumbit = handleSubmit;
+}
+
