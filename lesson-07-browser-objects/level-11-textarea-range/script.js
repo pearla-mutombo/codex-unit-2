386929e8eb805719cbf070ea94bde64dd8a56e36
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 15:48:56 -0400

    finished level 11 textarea & range

--- lesson-07-browser-objects/level-11-textarea-range/script.js
+++ lesson-07-browser-objects/level-11-textarea-range/script.js
@@ -1,12 +1,22 @@
 // Level 11 — textarea-range starter
 // TODO: in submit handler read textarea and range values via form.elements and convert range with Number()
+ const form = document.querySelector("sample-form");
+ if (form) {
+  form.onsubmit = handleSubmit;
+ }
 
 function handleSubmit(event) {
   // event.preventDefault();
+  event.preventDefault();
   // const form = event.target;
+  const form = event.target;
   // TODO: const message = form.elements.message.value;
+  const message = formElement.elements.message.value;
   // TODO: const volume = Number(form.elements.volume.value);
-  console.log("textarea & range scaffold");
+  const volume = Number(formElement.elements.value);
+
+  console.log("Message:", message);
+  console.log("Volume (as numner)", volume, "Type:" typeof volume);
 }
 
 // const form = document.getElementById('sample-form');
