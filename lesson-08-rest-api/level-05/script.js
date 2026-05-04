58dd307505d71fc6c0a316c78ecea37f745f5da0
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 22:18:52 -0400

    finished level 5 access the heanders/ starter script /display data

--- lesson-08-rest-api/level-05/script.js
+++ lesson-08-rest-api/level-05/script.js
@@ -1,13 +1,21 @@
 // Level 05 starter script
 // TODO: Fetch GET https://api.jsoning.com/mock/public/users, await response.json(), and read display the data.
 const form = document.getElementById("sample-form");
-const pUsername = document.getElementById("username");
-const pEmail = document.getElementById("email");
-const pId = document.getElementById("id");
 
-async function handleSubmit(e) {
-  e.preventDefault();
+
+form.onsubmit = handleSubmit();
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const userdata = await response.json();
+  const pUsername = document.getElementById("username");
+  const pEmail = document.getElementById("email");
+  const pId = document.getElementById("id");
+  pUsername.innerText = "Username:", username;
+  pEmail.innerText = "Email;",  email;
+  pId.innterText = "ID:", id;
+  
   // TODO: implement fetch and set the three elements' innerText
 }
 
-if (form) form.onsubmit = handleSubmit;
