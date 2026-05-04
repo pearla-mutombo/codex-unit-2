a3097f9a78d405a9c1a71b20edab264ed67b6637
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 19:19:13 -0400

    finished level 3 error handling

--- lesson-09-public-apis/level-03/script.js
+++ lesson-09-public-apis/level-03/script.js
@@ -1,10 +1,38 @@
 const btn3 = document.getElementById("fetch-btn");
-const errorEl3 = document.getElementById("error");
 
-if (btn3) {
-  btn3.addEventListener("click", async () => {
+  form.onsubmit = handleSubmit;
+
+
+  btn3.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const errorEl3 = document.getElementById("error");
+    const button = event.target;
+    const data = { Tryfetch = button.value };
+
+    try {
+      const response = await fetch("https://api.jsoning.com/mock/public/users", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+      });
+    
+      if (!response.ok) {
+        throw new Error("HTTP error! Status: ${response.status}");
+      }
+      const divData = await response.json();
+      console.log("Success:", divData);
+    
+    } catch (error) {
+    
+    if (errorEl3) {
+      errorEl3.innerText = "Resutl Error: ${error.message}";
+    }
+    console.error("Fetch failed:", error.message);
+    }
+   
+  });
+
+    
     // TODO: wrap fetch in try/catch
     // TODO: check response.ok and show friendly messages
-    console.log("level-03 fetch placeholder");
-  });
-}
