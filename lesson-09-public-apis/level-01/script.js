97bb68b00c6bd6823a3309e6b9fb7133530746ce
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 17:53:23 -0400

    finished level 1 login API

--- lesson-09-public-apis/level-01/script.js
+++ lesson-09-public-apis/level-01/script.js
@@ -1,13 +1,39 @@
 const form = document.getElementById("login-form");
-const errorEl = document.getElementById("error");
-const successEl = document.getElementById("success");
+
 
 if (form) {
+  form.onsubmit = handleSubmit;
+}
   form.addEventListener("submit", async (event) => {
+    event.preventDefault
+    const data = {
+      username: form.elements["username"].value,
+      passowrd: form.elements["password"].value
+    };
+    const response = await fetch("https://api.jsoning.com/mock/public/users", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    });
+
+    const divData = await response.json();
+
+    const errorEl = document.getElementById("error");
+    const successEl = document.getElementById("success");
+
+    if (errorEl) {
+      errorEl.innerText = "Result Error: ${divData.error}";
+    }
+    
+    if (successEl) {
+      successEl.innerText = "Result Success: ${divData.success}";
+    }
+
+    console.log("Parsed result:", divData);
     // TODO: call event.preventDefault()
     // TODO: build a data object from form fields
     // TODO: use fetch with async/await to POST to a dummy login endpoint
     // Do not commit real API keys — use test endpoints or mocks in tests
-    console.log("submit handler placeholder");
+  
   });
-}
+
