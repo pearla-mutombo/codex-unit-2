e78b7cfd9982a72b229bb81b4705883b94d5f1ae
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 22:59:28 -0400

    finished level 6 pre-process the form

--- lesson-08-rest-api/level-06/script.js
+++ lesson-08-rest-api/level-06/script.js
@@ -1,12 +1,31 @@
 // Level 06 starter script
 // TODO: Use form.elements (by index) to collect input values into a plain object.
 const form = document.getElementById("sample-form");
-const out = document.getElementById("data");
 
-function handleSubmit(e) {
+form.onsubmit = handleSubmit;
+
+async function handleSubmit(e) {
   e.preventDefault();
+  const form = event.target;
+  const data = {};
+  for (let i = 0; i < form.elements.length; i++)
+    const username = form.lements[i];
+    if (username) {
+      data[username] = username.value;
+    }
+  const out = document.getElementById("data");
+  if (out) {
+    out.innerText = JSON.stringify(data, null, 2);
+  }
+  
+  console.log("Form data collected into object:", data);
+
+  
+  // Build a 'data object from  form.elements using indexes.
+  // The elements property returns an array-like set of form controls.
+  // Use a standard for loop to iterate through form.elements by index
+  // Use input's name as the key and its current value as the value.
   // TODO: build a `data` object from form.elements using indexes (e.g. form.elements[0].value)
+  // Display the collected data in the <pre id="data"> tag
   // Example display: out.innerText = JSON.stringify(data, null, 2);
 }
-
-if (form) form.onsubmit = handleSubmit;
