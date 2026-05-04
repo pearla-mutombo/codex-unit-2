e1877ab15a292f979bd365a57fe41e0ad71099ba
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-03 21:23:38 -0400

    level 4 parsing the response

--- lesson-08-rest-api/level-04/script.js
+++ lesson-08-rest-api/level-04/script.js
@@ -1,11 +1,14 @@
 // Level 04 starter script
 // TODO: Await fetch and call response.json() to parse the body.
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
   // TODO: fetch and parse JSON, then set result.innerText with a short explanation
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const userdata = await response.json();
+  const pTag = document.getElementById("result");
+  pTag.innterText = "Parsing reads the server response and extracts the data from it. In this case, it converts it to an object."
 }
 
-if (form) form.onsubmit = handleSubmit;
