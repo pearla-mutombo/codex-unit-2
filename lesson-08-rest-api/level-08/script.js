ede28eebd503a660cf331b0fa32e8cf36a98d3d8
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 00:02:07 -0400

    finished lesson 8 send form data with POST

--- lesson-08-rest-api/level-08/script.js
+++ lesson-08-rest-api/level-08/script.js
@@ -1,12 +1,30 @@
 // Level 07 starter script
 // TODO: Build a data object from form.elements and use fetch POST to https://api.jsoning.com/mock/public/users
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
 
-async function handleSubmit(e) {
-  e.preventDefault();
+
+form.onsubmit = handleSubmit;
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = {
+    username: form.elements["username"].value,
+    email: form.elements["email"].value
+  };
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+ 
+  const pTag = document.getElementById("result");
+  if (pTag) {
+    pTag.innerText = "Posted - data is attached as a stringfield JSON body."
+
   // TODO: collect values via form.elements[0].value, form.elements[1].value, form.elements[2].value
   // build a data object and post it with fetch; then set result.innerText explaining how data is attached
+  }
 }
 
-if (form) form.onsubmit = handleSubmit;
+
