ee9df75d6433f344f706e1877d77bb528c13f040
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 00:10:51 -0400

    finished level 9 GET vs POST

--- lesson-08-rest-api/level-09/script.js
+++ lesson-08-rest-api/level-09/script.js
@@ -1,4 +1,9 @@
 // Level 09 starter script
 // TODO: Save a target element and set its innerText to briefly compare GET and POST.
-const result = document.getElementById("result");
+const pTag = document.getElementById("result");
+
+if (pTag) {
+    pTag.innerText = "GET is for reading data from a server. POST is for sending data to create update resources.";
+}
+
 // Example: result.innerText = 'GET is for reading; POST is for sending data.'
