113af462ae8f15fc677ceeeb89ba4f48013f434c
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-07 18:14:28 -0400

    finished level 7 - API Keys inserted an explanation about API keys into message

--- lesson-09-public-apis/level-07/script.js
+++ lesson-09-public-apis/level-07/script.js
@@ -1,7 +1,8 @@
 const messageEl = document.getElementById("message");
 
 // TODO: Select the message element and insert a short explanation about API keys
+// insert an explanation about API keys into message
 if (messageEl) {
   messageEl.innerText =
-    "API keys identify your app to a service; do not commit them publicly.";
+    "API keys identify your app to a service; do not commit them publicly. its keys are unique credentials that identify your app to a service and tracks your usage; they are secrets and should never be shared publicly or committed to repositories";
 }
