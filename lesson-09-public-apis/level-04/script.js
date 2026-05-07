ef79c844ca9e7e725d120c8a2f10d44ff11fc8fe
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-06 22:55:33 -0400

    made corrections on level 4 query params - on script.js, index.html and added a style.css sheet

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -10,16 +10,13 @@ function handleSubmit(event) {
   };
   const queryString = new URLSearchParams(data);
   console.log("This is the query string", queryString);
-}
-    // TODO: preventDefault, build a data object, use URLSearchParams
-    // TODO: append query string to GET request URL
-  
-const formTag = document.getElementById("queryForm");
-formTag.onsubmit = handleSubmit;
 
-function handleSubmit(event) {
-  event.preventDefault();
+  // Append the query string to the URL to satisfy the final test
+  // This triggers the GET request by updating the browser's address bar
+  window.location.search = queryString.toString();
+}
+  // TODO: preventDefault, build a data object, use URLSearchParams
+  // TODO: append query string to GET request URL
   // TODO: Read form values with formTag.elements and create `data` object
   // TODO: Use `new URLSearchParams(data)` to create a query string and save to `query`
   // TODO: console.log(query)
-}
