664df6dfa5be53c9d1200cd4abb1931cbee01fad
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-12 14:06:09 -0400

    finished level 3 GET with query parameters.

--- lesson-10-postman/level-03/script.js
+++ lesson-10-postman/level-03/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 03: experiment with query parameters.
 
 // Students may optionally replicate the request in code and log differences.
-console.log("TODO: log one documentation fact here");
+console.log("for categories to return the questions form i can choose the different levels of categories=sciene,film_and_tv. Or i can choose one. for difficulties levels it can be made easy,medium,hard.");
