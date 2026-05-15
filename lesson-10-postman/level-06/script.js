6bb936f4845a5f6a3a3c8f038d99099904ccae01
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-14 22:01:24 -0400

    finished level 6 - API GET practice

--- lesson-10-postman/level-06/script.js
+++ lesson-10-postman/level-06/script.js
@@ -2,4 +2,11 @@
 
 // Students should replace the URL below with their chosen API endpoint.
 // TODO: fetch('https://api.example.com/endpoint')
-console.log("TODO: log one documentation fact here");
+const data = {
+    message: "",
+};
+const dataString = new URLSearchParams(data);
+const response = await fetch("https://potterapi-fedeperin.vercel.app" + "?" + dataString);
+const result = await response.json();
+result.message = "",
+console.log("A REST Harry Potter API with images and information about books, characters and spells. Translated to many languages");
