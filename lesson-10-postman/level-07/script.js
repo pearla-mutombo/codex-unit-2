24fbc4e1a3ecd9612bcae879531d67231904b8f4
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-14 22:35:35 -0400

    finished level 7 - API POST practice

--- lesson-10-postman/level-07/script.js
+++ lesson-10-postman/level-07/script.js
@@ -1,4 +1,17 @@
 // TODO: Starter for Level 07: API POST practice.
 
 // Students should construct a JSON payload and send it to their chosen API.
-console.log("TODO: log one documentation fact here");
+const response = await fetch("https://zenquotes.io/", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    title: "Learning API POST Requests",
+    body: "This request sends raw JSON data to a target endpoint.",
+    userId: 1,
+  }),
+});
+const result = await response.json();
+
+console.log(
+  "Zenquotes.io is a simple API that can be used to fetch quotes from infuential figures throughout history into JSON format. While it's not hard to find a database of inspirational quotes online, most of them require registration or just plain suck.",
+);
