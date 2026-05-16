1052f4909b94f2a683fe333f2f5581ab2bffbc1f
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-16 15:14:30 -0400

    corrected level 4 script

--- lesson-10-postman/level-04/script.js
+++ lesson-10-postman/level-04/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 04: POST request basics.
 
 // Students should use Postman to send a POST; this file is a placeholder
-console.log(" the respose status in the dummyjson.com/auth/login URL read 400 Bad Request, the server could not understand the request. Maybe a bad syntax? and in the dummyjson.com/docs/auth the docs for required fields reads 404 Not found, Request resource could not be found");
+console.log(" with POST method we need to include a body in the request. otherwise, it runs an error message of 404 status code.");
