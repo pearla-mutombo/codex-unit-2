c1aa86b5e00d84d31c270257b502a35ea83519b1
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-16 15:22:25 -0400

    corrected level 5 script

--- lesson-10-postman/level-05/script.js
+++ lesson-10-postman/level-05/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 05: POST with JSON payloads.
 
 // Placeholder: students should set Content-Type and send JSON in Postman.
-console.log("DummyJSON can be used with any type of front end project that needs products, carts, users, todos or any dummy data in JSON format");
+console.log("It provides different user's personal information, that goes above and beyond the normal scope of information that's needed.");
