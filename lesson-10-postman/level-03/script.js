cbef1df251ea5ad6703bb2c1058e3c1e412a45d7
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-16 14:45:40 -0400

    corrected level 3 script

--- lesson-10-postman/level-03/script.js
+++ lesson-10-postman/level-03/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 03: experiment with query parameters.
 
 // Students may optionally replicate the request in code and log differences.
-console.log("for categories to return the questions form i can choose the different levels of categories=sciene,film_and_tv. Or i can choose one. for difficulties levels it can be made easy,medium,hard.");
+console.log("With each catergories question, users are provided with four different answers. Where one is the true correct answers and the other three are incorrect answers.");
