7a9f5806898757f999353adb5929f445839e1681
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-16 16:57:22 -0400

    corrected level 8 script

--- lesson-10-postman/level-08/script.js
+++ lesson-10-postman/level-08/script.js
@@ -1,41 +1,46 @@
 // TODO: Starter for Level 08: implement submit handler.
-const form = document.getElementById("api-form");
-const inputEl= document.getElementById("input");
+const formTag = document.getElementById("api-form");
+const inputEl = document.getElementById("input");
 const outputEl = document.getElementById("result");
 
-form.onsubmit = handleSubmit;
+formTag.onsubmit = handleSubmit;
 
 // implement submit handler
-async function handleSubmit(event){
-    event.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
 
-    // visible feeback for the user during the request
-    outputEl.style.color = "black";
-    outputEl.innerText = "Sending request..";
+  // visible feeback for the user during the request
+  outputEl.style.color = "black";
+  outputEl.innerText = "Sending request..";
 
-    const form = event.target;
-    const requestData = {
-        title: inputEl,
-        body: "Sent via API Challenge Form",
-        userId: 1
-    };
-    const response = await fetch("https://typicode.com", {
-      methond: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(requestData)  
+  // const form = event.target;
+
+  const requestData = {
+    title: inputEl.value,
+    body: "Sent via API Challenge Form",
+    userId: 1,
+  };
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=UTF-8" },
+      body: JSON.stringify(requestData),
     });
-    const data =  await response.json();
+    const result = await response.json();
     // parsing the reponse into separate variables
-    const responseId = data.id;
-    const responseTittle = data.title;
+    const resultId = result.id;
+    const resultTittle = result.title;
 
     //providing clear sucess feedback to the user
     outputEl.style.color = "green";
-    outputEl.innerText = "Success! Created post" + responseId + "with value" + responseTittle;
+    outputEl.innerText = "Success! Created post " + resultId + " with " + resultTittle;
     // reseting the form fields
-    form.reset();
-    //providing clear user feedback if/for failures and error messages
-    outputEl.style.color = "red";
-    outputEl.innerText = "Error" + error.message;
+    formTag.reset();
 
+  } catch (error) {
+    //providing clear user feedback if/for failures and error messages
+    console.error(error);
+    outputEl.style.color = "red";
+    outputEl.innerText = error.message;
+  }
 }
